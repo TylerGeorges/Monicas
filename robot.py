6391fececd5d114101ea18f9d8e5f3a0834f7edd
Poul Poulsen <poul@encore.dk>
2017-10-17 09:49:01 +0200

    Update robot.py

--- robot.py
+++ robot.py
@@ -17,3 +17,37 @@ class Robot:
         self.read_delay = 0.2
         self.version = '0.2.3 (alpha)'
         self.COMMANDS_MAP = self.build_commands_map()
+        
+    def build_commands_map(self):
+        return {
+            '!help': {
+                'function': 'command_help',
+                'example': '!help',
+                'description': 'Displays a list of valid commands'
+            },
+            '!wiki': {
+                'function': 'command_wiki',
+                'example': '!wiki <search phrase>',
+                'description': 'returns a single search result from wiki (via DuckDuckGo)'
+            },
+            '<@{}>'.format(self.bot_id): {
+                'function': 'command_robot',
+                'example': '@{} <question>'.format(self.name),
+                'description': 'chat with {}'.format(self.name)
+            },
+            '!demo': {
+                'function': 'command_demo',
+                'example': '!demo <question>',
+                'description': 'play with the current command in demo mode'
+            },
+            '!uptime': {
+                'function': 'command_uptime',
+                'example': '!uptime',
+                'description': 'Show <@{}>\'s uptime'.format(self.name)
+            },
+            '!version': {
+                'function': 'command_version',
+                'example': '!version',
+                'description': 'Show <@{}>\'s version'.format(self.name)
+            }
+        }
