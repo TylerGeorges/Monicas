bb35a9e3e426815f5236d06514d65d2f5ca61b43
Poul Poulsen <poul@encore.dk>
2017-10-17 09:47:36 +0200

    Update robot.py

--- robot.py
+++ robot.py
@@ -5,3 +5,15 @@ import logging
 from slackclient import SlackClient
 from duckduckpy import query
 import wolframalpha
+
+class Robot:
+
+    def __init__(self, name='robot'):
+        self.name = name
+        self.slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+        self.bot_id = self.get_bot_id()
+        self.wa_client = wolframalpha.Client(os.environ.get('WA_TOKEN'))
+        self.bot_start_time = time.time()
+        self.read_delay = 0.2
+        self.version = '0.2.3 (alpha)'
+        self.COMMANDS_MAP = self.build_commands_map()
